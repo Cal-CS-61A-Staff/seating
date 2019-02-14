96b390800b18f47256f8894cd39be40647b350c3
Mehrdad <>
2019-02-13 16:30:00 -0800

    Clarify download_bcourses_photos.py and handle encodings properly

--- download_bcourses_photos.py
+++ download_bcourses_photos.py
@@ -6,17 +6,28 @@ import errno
 import json
 import mimetypes
 import os
-import queue
 import sys
 import threading
-try: import urllib.request as urllib_request  # Python 3
-except ImportError: import urllib2 as urllib_request  # Python 2
+
+try: from queue import Queue  # Python 3
+except ImportError: from Queue import Queue  # Python 2
+
+try: import urllib.request as urllib2  # Python 3
+except ImportError: import urllib2  # Python 2
 
 def urlopen(url, *args, **kwargs):
 	cookie = kwargs.pop('cookie', None)
-	opener = urllib_request.build_opener()
+	opener = urllib2.build_opener()
 	if cookie is not None: opener.addheaders.append(('Cookie', cookie))
 	return opener.open(url, *args, **kwargs)
+
+def get_content_charset(headers):
+	try: get_param = headers.get_param  # Python 3
+	except AttributeError: get_param = headers.getparam  # Python 3
+	return get_param("charset") or 'utf-8'
+
+def read_http_response_as_json(response):
+	return json.load(response, encoding=get_content_charset(response.headers))
 
 def fetcher(q):
 	mime_type_ranks = ['.jpg', '.png', '.tif', '.bmp']
@@ -51,7 +62,7 @@ def fetcher(q):
 def main(program, bcourses_cookie):
 	# 1. Put all your students' emails into a text file (one email per line), let's call this "Student-Emails.txt".
 	# 2. Now press F12 to open the debugging tools, go to the Network tab, and then navigate to the course roster.
-	# 3. Find a GET request to junction.berkeley.edu and copy its "Cookie" field. The cookie should begin with "_calcentral_session".
+	# 3. Find a GET request to junction.berkeley.edu and copy its "Cookie" field from the "Headers" tab (NOT the "Cookies" tab). You need the cookie string beginning with "_calcentral_session".
 	#    (I used the "Cookie" field in the request headers, but you can also try the "Set-Cookie" field in the response headers.)
 	# 4. Call this script and pass it all the above information in the following order (notice that "Student-Emails.txt" is PIPED in):
 	#    python "this-script.py" "_calcentral_session=ABCDEFGHIJK" < "Student-Emails.txt"
@@ -60,13 +71,13 @@ def main(program, bcourses_cookie):
 	nthreads = max(int(nthreads), 1)
 	sys.stderr.writelines(["Downloading course info..."]); sys.stderr.flush()
 	server = "https://junction.berkeley.edu"
-	course_info = json.loads(urlopen(server + "/api/academics/rosters/canvas/embedded", cookie=bcourses_cookie).read())
+	course_info = read_http_response_as_json(urlopen(server + "/api/academics/rosters/canvas/embedded", cookie=bcourses_cookie))
 	course_id = course_info['canvas_course']['id']
 	course_name = course_info['canvas_course']['name']
 	course_students = course_info['students']
 	sys.stderr.writelines([" %s: %s\n" % (course_id, course_name)]); sys.stderr.flush()
 	roster = dict(map(lambda student: (student['email'], student), course_students))
-	q = queue.Queue()
+	q = Queue()
 	threads = list(map(lambda i: threading.Thread(target=fetcher, args=(q,)), range(nthreads)))
 	for t in threads: t.setDaemon(True); t.start()
 	sys.stderr.writelines(["Reading student emails..."]); sys.stderr.flush()
